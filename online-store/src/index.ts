684183a3b45ae3ef3f1e37720dab56f0e393f019
asya2320 <64478347+asya2320@users.noreply.github.com>
2022-12-20 00:12:14 +0300

    feat: add pages for cart and products and input range

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,6 +1,8 @@
 import './index.css';
 import dataProducts from './components/dataProducts';
 import startPage from './components/startPage';
+import cartPage from "./components/cartPage";
+import productPage from "./components/productPage";
 console.log(dataProducts);
 
 let main = <HTMLElement>document.querySelector('.main');
@@ -55,3 +57,80 @@ for (let i=0; i<arrayBrand.length; i++){
     <span>(5/5)</span>`
     filterBrand.append(checkboxBrand);
 }
+
+let cart = <HTMLElement>document.querySelector(".basket-img");
+let footer = <HTMLElement>document.querySelector(".footer");
+
+cart.addEventListener("click", () => {
+  main.innerHTML = cartPage;
+  main.classList.add("main-cart");
+  footer.classList.add("footer-product");
+  let toModal = <HTMLElement>document.querySelector(".toModal");
+  let modalBg = <HTMLElement>document.querySelector(".modal-background");
+  let modalWindow = <HTMLElement>document.querySelector(".modal-window");
+  let closeModal = <HTMLElement>document.querySelector(".modal-close");
+
+  toModal.addEventListener("click", () => {
+    modalBg.style.display = "block";
+    modalWindow.style.display = "flex";
+  });
+
+  closeModal.addEventListener("click", () => {
+    modalBg.style.display = "none";
+    modalWindow.style.display = "none";
+  });
+});
+
+let allCards: HTMLCollection = productsCards.children;
+if (allCards != undefined) {
+  for (let i = 0; i < dataProducts.length; i++) {
+    allCards[i].addEventListener("click", () => {
+      main.innerHTML = productPage;
+      main.classList.add("main-cart");
+      main.style.flexDirection = "column";
+      footer.classList.add("footer-product");
+
+      let title = <HTMLElement>document.querySelector(".product-title");
+      title.innerText = dataProducts[i].title;
+
+      let description = <HTMLElement>(
+        document.querySelector(".product-description")
+      );
+      description.innerText = dataProducts[i].description;
+
+      let rating = <HTMLElement>document.querySelector(".product-rating");
+      rating.innerText = String(dataProducts[i].rating);
+
+      let stock = <HTMLElement>document.querySelector(".product-stock");
+      stock.innerText = String(dataProducts[i].stock);
+
+      let brand = <HTMLElement>document.querySelector(".product-brand");
+      brand.innerText = dataProducts[i].brand;
+
+      let category = <HTMLElement>document.querySelector(".product-category");
+      category.innerText = dataProducts[i].category;
+
+      let discount = <HTMLElement>(
+        document.querySelector(".product-discountPercentage")
+      );
+      discount.innerText = String(dataProducts[i].discountPercentage);
+
+      let path = <HTMLElement>document.querySelector(".path-product");
+      path.innerText = `Store > ${dataProducts[i].category} > ${dataProducts[i].brand} > ${dataProducts[i].title}`;
+
+      let main_page = <HTMLElement>document.querySelector(".photos-main");
+      main_page.style.backgroundImage = `url('${dataProducts[i].images[0]}')`;
+      for (let j = 0; j < 3; j++) {
+        let k = <HTMLElement>document.getElementsByClassName("photo-other")[j];
+        k.style.backgroundImage = `url('${dataProducts[i].images[j]}')`;
+        k.style.backgroundSize = "cover";
+        k.addEventListener("click", () => {
+          main_page.style.backgroundImage = `url('${dataProducts[i].images[j]}')`;
+        });
+      }
+      let cost = <HTMLElement>document.querySelector(".cost-product");
+      cost.innerText = `€${dataProducts[i].price}`;
+    });
+  }
+}
+
