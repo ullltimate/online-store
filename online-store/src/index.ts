a46e73bb9f2466d4312f9af00be08bb8aa735668
ullltimate <ullltimate.zp@gmail.com>
2022-12-30 17:31:30 +0300

    feat: add routing for home and cart

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -2,20 +2,58 @@ import './index.css';
 import dataProducts from './components/dataProducts';
 import home from './components/startPage';
 import cartProduct from "./components/cartPage";
+import productPage from "./components/productPage";
+import page404 from "./components/page404";
 
+const getRoute = () => {
+    const routes = [
+        {
+            path: '/',
+            data: home,
+        },
+        {
+            path: '/cart',
+            data: cartProduct,
+        },
+        {
+            path: '/404',
+            data: page404,
+        },
+        {
+            path: '/product',
+            data: productPage,
+        },
+    ];
 
-let main = <HTMLElement>document.querySelector('.main');
-home();
+    let handleLocation = () => {
+        const html = routes.find((route) => route.path === location.pathname) || routes.at(-1);
 
-let logo = <HTMLElement>document.querySelector('.logo');
+        const main = <HTMLElement | null> document.querySelector('.main');
 
-logo.addEventListener('click', () => {
-  home();
-});
+        if (main !== null) {
+            const getPageContent: any = html?.data
+            getPageContent(main);
+        }
 
-let cart = <HTMLElement>document.querySelector(".basket-img");
+        window.addEventListener('popstate', handleLocation);
+        window.addEventListener('DOMContentLoaded', handleLocation);
+    }; 
+    handleLocation();
+}
 
-cart.addEventListener("click", () => {
-  cartProduct();
-});
+getRoute()
+
+//home();
+
+//let logo = <HTMLElement>document.querySelector('.logo');
+//
+//logo.addEventListener('click', () => {
+//  home();
+//});
+
+//let cart = <HTMLElement>document.querySelector(".basket-img");
+//
+//cart.addEventListener("click", () => {
+//  cartProduct();
+//});
 
